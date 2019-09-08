68cc436b711c69c1be89c21acbd8dcd51d1ac483
kai <kai@kaideMacBook-Pro.local>
2019-09-08 19:55:59 +0800

    Update index.ts

--- src/jsonrpc/index.ts
+++ src/jsonrpc/index.ts
@@ -1,12 +1,12 @@
 import {Context} from 'koa';
 import JsonRPC from './JsonRPC';
-import {IJsonRPCRequest} from './IJsonRPC';
+import {IJsonRPCRequest, IJsonRPCResponse} from './IJsonRPC';
 
 /**
  * 根据 jsonrpc 处理函数，根据上下文数据，返回对应的响应
  * @param ctx
  */
-async function jsonrpcHandler(ctx: Context) {
+async function jsonrpcHandler(ctx: Context): Promise<IJsonRPCResponse | IJsonRPCResponse[]> {
     const requestBody: IJsonRPCRequest | IJsonRPCRequest[] = ctx.request.body;
     if (!Array.isArray(requestBody)) {
         const jsonrpc: JsonRPC = new JsonRPC(requestBody);
@@ -14,7 +14,7 @@ async function jsonrpcHandler(ctx: Context) {
     }
 
     // TODO 如果是批量请求
-
+    return [];
 }
 
 export default jsonrpcHandler;
