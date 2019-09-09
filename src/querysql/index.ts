052b6be44670b769ef9b1e44bc56ca37d5fc61b5
kai <kai@kaideMacBook-Pro.local>
2019-09-09 09:52:28 +0800

    已经连通数据库

---
+++ src/querysql/index.ts
@@ -0,0 +1,35 @@
+import {createConnection, MysqlError} from 'mysql';
+
+const connection = createConnection({
+    host: '172.81.210.92',
+    user: 'root',
+    password: 'LiuKai@#&**&#@',
+    database: 'test',
+});
+
+/**
+ *
+ */
+function querysql(options: string, values: string[]): Promise<{ results: object | object[], fields: any }> {
+
+    return new Promise((resolve, reject) => {
+        connection.connect();
+
+        connection.query(options, values,
+            (error, results, fields) => {
+                // console.log(error, results, fields);
+                if (error) {
+                    return reject(error);
+                }
+                resolve({results, fields});
+                // console.log('The solution is: ', results[0].solution);
+            });
+
+        connection.end();
+    });
+
+}
+
+export default querysql;
+
+
